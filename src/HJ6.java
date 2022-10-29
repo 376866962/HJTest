a80e82c3750fc69ba275d836ff2eac0c453f4299
SUO <376866962@qq.com>
2022-10-29 08:10:40 +0800

    提交测试

---
+++ src/HJ6.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class HJ6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        while (sc.hasNext()){
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                map.put(a, map.getOrDefault(a, 0) + b);
+            }
+            Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+//            while (iterator.hasNext()){
+//                Map.Entry<Integer, Integer> entry = iterator.next();
+//                System.out.println(entry.getKey()+" "+entry.getValue());
+//            }
+        }
+    }
+}
